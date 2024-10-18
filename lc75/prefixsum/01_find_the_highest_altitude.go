c035a5c5e5cd088a5c9d3efea3a3e6d5160bba82
jparker <devel@jasonparker.info>
2024-10-18 06:21:03 +0000

    add comments to code

--- lc75/prefixsum/01_find_the_highest_altitude.go
+++ lc75/prefixsum/01_find_the_highest_altitude.go
@@ -1,12 +1,34 @@
 package prefixsum
 
+// largestAltitude calculates the highest altitude reached based on a series of altitude gains.
+// It takes a slice of integers 'gain' representing the net altitude change between two points.
+// The function returns the maximum altitude reached as an integer.
+//
+// The initial altitude is assumed to be 0, and each element in 'gain' represents the
+// change in altitude from the previous point. The function keeps track of the current
+// altitude and updates the maximum altitude whenever a new peak is reached.
+//
+// Parameters:
+//   - gain: A slice of integers representing altitude changes.
+//
+// Returns:
+//   - The highest altitude reached as an integer.
 func largestAltitude(gain []int) int {
+	// Initialize variables to track the maximum altitude (maxAlt) and current altitude (alt)
 	var maxAlt, alt int = 0, 0
+
+	// Iterate through each altitude gain in the input slice
 	for _, g := range gain {
+		// Update the current altitude by adding the gain
 		alt += g
+
+		// If the current altitude is higher than the maximum recorded altitude,
+		// update the maximum altitude
 		if maxAlt < alt {
 			maxAlt = alt
 		}
 	}
+
+	// Return the highest altitude reached
 	return maxAlt
 }
--- lc75/prefixsum/01_find_the_highest_altitude_test.go
+++ lc75/prefixsum/01_find_the_highest_altitude_test.go
@@ -2,6 +2,8 @@ package prefixsum
 
 import "testing"
 
+// largestGainTestCases is a slice of test cases for the largestAltitude function.
+// Each test case contains a name, an input slice of integers, and the expected output.
 var largestGainTestCases = []struct {
 	Name  string
 	Input []int
@@ -11,6 +13,11 @@ var largestGainTestCases = []struct {
 	{"2", []int{-4, -3, -2, -1, 4, 3, 2}, 0},
 }
 
+// TestLargestGainTestCases runs through a series of test cases to verify the correctness
+// of the largestAltitude function. It iterates over the predefined largestGainTestCases,
+// applying the largestAltitude function to each input and comparing the result with the
+// expected output. If any discrepancies are found, it reports an error using the testing
+// framework, providing details about the failed test case.
 func TestLargestGainTestCases(t *testing.T) {
 	for _, tc := range largestGainTestCases {
 		got := largestAltitude(tc.Input)
