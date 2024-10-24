9ffb9e0c6289c29c84a6e7a45fc3b3cbadb44baa
jparker <devel@jasonparker.info>
2024-10-24 15:44:00 +0000

    created documentation

--- lc75/hashmapset/02_unique_number_of_occurrences.go
+++ lc75/hashmapset/02_unique_number_of_occurrences.go
@@ -1,5 +1,15 @@
 package hashmapset
 
+// uniqueOccurrences determines if all numbers in the input array appear unique number of times
+// For example: [1,2,2,1,1,3] has 1 appearing 3 times, 2 appearing 2 times, and 3 appearing 1 time
+// Therefore uniqueOccurrences would return true since 1,2,3 times are all unique frequencies
+// If [1,2,2,1,1,3,3] was passed, it would return false since 3 appears 2 times which is not unique
+// 
+// Parameters:
+//   arr []int: Array of integers to check for unique occurrence counts
+//
+// Returns:
+//   bool: true if all numbers appear unique number of times, false otherwise
 func uniqueOccurrences(arr []int) bool {
 	table := map[int]int{}
 	for _, n := range arr {
